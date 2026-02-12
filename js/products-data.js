cc967eb95d36c36d3ca979c9816d2175e2d919e7
bwmatx <wibowopotrek5@gmail.com>
2026-02-13 01:47:08 +0700

    Update products-data.js

--- js/products-data.js
+++ js/products-data.js
@@ -6,7 +6,7 @@ const products = [
         category: 'Tradisional',
         price: 27500,
         description: 'Kue kering klasik dengan isian selai nanas yang manis dan lembut, dibuat dengan resep turun-temurun.',
-        image: '../img/nastar.png'
+        image: 'img/nastar.png'
     },
     {
         id: 'kastengel',
@@ -14,7 +14,7 @@ const products = [
         category: 'Tradisional',
         price: 27500,
         description: 'Kue keju renyah dengan taburan keju yang melimpah, gurih dan renyah di setiap gigitan.',
-        image: '../img/kastengel.jpg'
+        image: 'img/kastengel.jpg'
     },
     {
         id: 'putri-salju',
@@ -22,7 +22,7 @@ const products = [
         category: 'Modern',
         price: 27500,
         description: 'Kue lembut yang meleleh di mulut dengan lapisan gula halus, tekstur yang lembut dan manis.',
-        image: '../img/putri-salju.jpg'
+        image: 'img/putri-salju.jpg'
     },
     {
         id: 'kue-kacang',
@@ -30,7 +30,7 @@ const products = [
         category: 'Tradisional',
         price: 25000,
         description: 'Kue kacang tanah yang renyah dengan aroma kacang sangrai yang khas, cocok untuk teman ngopi.',
-        image: '../img/kue-kacang.jpg'
+        image: 'img/kue-kacang.jpg'
     },
     {
         id: 'cookies-coklat',
@@ -38,7 +38,7 @@ const products = [
         category: 'Modern',
         price: 25000,
         description: 'Cookies coklat renyah di luar dan lembut di dalam.',
-        image: '../img/kue-coklat.jpeg'
+        image: 'img/kue-coklat.jpeg'
     },
     {
         id: 'madu-mongso',
@@ -46,6 +46,6 @@ const products = [
         category: 'Tradisional',
         price: 17500,
         description: 'Kue tradisional dengan rasa manis dari madu dan aroma harum mongso yang khas.',
-        image: '../img/madumongso.webp'
+        image: 'img/madumongso.webp'
     }
 ];
